9c4068432475ce47d4bb6110404376cccf5dfe7d
Mathews Peter <mathewspeter27@gmail.com>
2021-04-10 23:35:12 +0530

    references

---
+++ references.cpp
@@ -0,0 +1,23 @@
+/*
+ * references.cpp
+ *
+ *  Created on: 10 Apr 2021
+ *      Author: mathewspeter
+ */
+
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a=3;
+	int &b=a;
+	cout<<a<<endl;
+
+	cout<<b<<endl;
+
+}
+
+
+
