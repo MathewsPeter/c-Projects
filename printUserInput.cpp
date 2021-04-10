c4c9282414771fc3915b2185af43ee0f7a7ba86e
Mathews Peter <mathewspeter@Mathewss-MacBook-Air.local>
2021-04-10 12:15:14 +0530

    printUserInput

---
+++ printUserInput.cpp
@@ -0,0 +1,16 @@
+/*
+ * printUserInput.cpp
+ *
+ *  Created on: 10 Apr 2021
+ *      Author: mathewspeter
+ */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	cout << "The number is " << n;
+	return 0;
+}
