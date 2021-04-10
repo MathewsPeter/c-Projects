935a190690a7dd191377df57c3a821bad3f9d33b
Mathews Peter <mathewspeter@Mathewss-MacBook-Air.local>
2021-04-10 18:52:52 +0530

    ascii

---
+++ ascii.cpp
@@ -0,0 +1,22 @@
+/*
+ * ascii.cpp
+ *
+ *  Created on: 10 Apr 2021
+ *      Author: mathewspeter
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char a='a';
+	char i;
+	for(i='a';i<='j';++i)
+	{
+		cout<<a<<" in ASCII is "<<int(a)<<endl;
+		a++;
+	}
+}
+
+
