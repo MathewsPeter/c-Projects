84215cd69d8b7e64ae821c962156171147970e15
Mathews Peter <mathewspeter@Mathewss-MacBook-Air.local>
2021-04-10 13:30:23 +0530

    swap

---
+++ swap.cpp
@@ -0,0 +1,23 @@
+/*
+ * swap.cpp
+ * interchange 2 numbers
+ *
+ *  Created on: 10 Apr 2021
+ *      Author: mathewspeter
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a=2,b=9;
+	cout<<a<<","<<b<<"\n";
+	a = a+b;
+	b = a-b;
+	a = a-b;
+	cout<<a<<","<<b;
+}
+
+
+
