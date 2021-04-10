6a9377198eabdade4a5ef176ded96c1b978db912
Mathews Peter <mathewspeter@Mathewss-MacBook-Air.local>
2021-04-10 13:37:19 +0530

    oddeven

---
+++ oddeven.cpp
@@ -0,0 +1,23 @@
+/*
+ * oddeven.cpp
+ * checks if an integer is a multiple of 2 or not
+ *
+ *  Created on: 10 Apr 2021
+ *      Author: mathewspeter
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	cin>>a;
+	if(a%2)
+		cout<<"odd";
+	else cout<<"Even";
+	return 0;
+
+}
+
