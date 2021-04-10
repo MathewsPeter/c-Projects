f0b83d9b4df4046ecd5f4fddedb4ca56b4e5480b
Mathews Peter <mathewspeter@Mathewss-MacBook-Air.local>
2021-04-10 13:09:07 +0530

    sizeOfDataTypes

---
+++ sizeofDataTypes.cpp
@@ -0,0 +1,21 @@
+/*
+ * sizeofDataTypes.cpp
+ * print the size in bytes of different data types
+ *
+ *  Created on: 10 Apr 2021
+ *      Author: mathewspeter
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"\n size of int: "<<sizeof(int);
+	cout<<"\n size of float: "<<sizeof(float);
+	cout<<"\n size of char: "<<sizeof(char);
+	cout<<"\n size of double: "<<sizeof(double);
+	return 0;
+}
+
+
