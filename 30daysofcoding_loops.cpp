9c2302d80d325cade8980181ef4e4ee5c96aba79
Mathews Peter <mathewspeter27@gmail.com>
2021-04-26 20:42:51 +0530

    30dayscoding_loop

---
+++ 30daysofcoding_loops.cpp
@@ -0,0 +1,10 @@
+/*
+ * 30daysofcoding_loops.cpp
+ *
+ *  Created on: 26 Apr 2021
+ *      Author: mathewspeter
+ */
+
+
+    for(int i = 1; i<=10; ++i)
+        cout<<n<<" x "<<i<<" = "<<(i*n)<<endl;
